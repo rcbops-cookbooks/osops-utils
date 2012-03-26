8e30d57d59cedffb44590aeadf460e11dea56679
rpedde <ron@pedde.com>
2012-03-26 02:37:10 +0000

    first pass

--- libraries/ip_location.rb
+++ libraries/ip_location.rb
@@ -1,20 +1,25 @@
 #!/usr/bin/env ruby
 
+require "chef/search/query"
 require "ipaddr"
+require "pp"
 
-class IpLocation
+class Chef::Recipe::IPManagement
   # find the local ip for a host on a specific network
-  def get_ip_for_net(network, node=@node)
+  def self.get_ip_for_net(network, node)
     if not node[:osops_networks][network] then
       Chef::Log.err("Can't find network #{network}")
     end
 
     net = IPAddr.new(node[:osops_networks][network])
     node[:network][:interfaces].each do |interface|
-      interface[:addresses].keys.each do |address|
-        addr=IPAddr.new(address)
-        if net.include?(addr) then
-          return address
+      pp interface
+      interface[1][:addresses].each do |k,v|
+        if v[:family] == "inet6" or v[:family] == "inet" then
+          addr=IPAddr.new(k)
+          if net.include?(addr) then
+            return k
+          end
         end
       end
     end    
@@ -23,16 +28,32 @@ class IpLocation
   end
 
   # find the realserver ips for a particular role
-  def get_ips_for_role(role, network)
+  def self.get_ips_for_role(role, network, node)
     if Chef::Config[:solo] then
+      return node[:controller_ipaddress]
     else
+      candidates, something, result_count = Chef::Search::Query.new.search(:node, "chef_environment:#{node.chef_environment} AND role:#{role}")
+
+      if result_count == 0 then
+        Chef::Log.err("Can't find any candidates for roled #{role} in environment #{node.chef_environment}")
+      end
+
+      return candidates.map { |x| get_ip_for_net(network, x) }
     end
   end
 
-  # find the loadbalancer ips for a particular role
-  def get_access_ips_for_role(role, network)
+  # find the loadbalancer ip for a particular role
+  def self.get_access_ips_for_role(role, network, node)
     if Chef::Config[:solo] then
+      return node[:controller_ipaddress]
     else
+      candidates, something, result_count = Chef::Search::Query.new.search(:node, "chef_environment:#{node.chef_environment} AND role:#{role}")
+      if result_count == 1 then
+        return get_ip_for_net(network, candidates[0], node)
+      else
+        return node[:osops_vips][role]
+      end
     end
   end
 end
+
