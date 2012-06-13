5f3883a46cac4358e5f8c76d3bc740c9a5bfe64e
rpedde <ron@pedde.com>
2012-06-13 16:44:37 -0500

    Add functions for getting interface by network tag

--- libraries/ip_location.rb
+++ libraries/ip_location.rb
@@ -49,9 +49,47 @@ module RCB
 
   # stub until we can migrate out the IPManagement stuff
   def get_ip_for_net(network, nodeish = nil)
-    nodeish = node unless nodeish
+    _, ip = get_if_ip_for_net(network nodeish)
+    return ip
+  end
 
-    Chef::Recipe::IPManagement.get_ip_for_net(network, nodeish)
+  def get_if_for_net(network, nodeish = nil)
+    iface, _ = get_if_ip_for_net(network, nodeish)
+    return iface
+  end
+
+  def get_if_ip_for_net(network, nodeish = nil)
+    nodish = node unless nodeish
+
+     if network == "all"
+      return "0.0.0.0"
+    end
+
+    if network == "localhost"
+      return "127.0.0.1"
+    end
+
+    if not (node.has_key?("osops_networks") and node["osops_networks"].has_key?(network)) then
+      error = "Can't find network #{network}"
+      Chef::Log.error(error)
+      raise error
+    end
+
+    net = IPAddr.new(node["osops_networks"][network])
+    node["network"]["interfaces"].each do |interface|
+      interface[1]["addresses"].each do |k,v|
+        if v["family"] == "inet6" or v["family"] == "inet" then
+          addr=IPAddr.new(k)
+          if net.include?(addr) then
+            return [interface[0], k]
+          end
+        end
+      end
+    end
+
+    error = "Can't find address on network #{network} for node"
+    Chef::Log.error(error)
+    raise error
   end
 
   def get_bind_endpoint(server, service, nodeish=nil)
