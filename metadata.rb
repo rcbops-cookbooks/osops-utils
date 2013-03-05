6414fefcd0e8e3c60633d29d1d58226165671c79
rcbjenkins <rcb-deploy@lists.rackspace.com>
2013-03-05 15:05:18 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "osops@lists.launchpad.net"
 license          "Apache 2.0"
 description      "Installs/Configures osops-utils"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.14"
+version          "1.0.15"
 
 %w{ centos ubuntu }.each do |os|
   supports os
