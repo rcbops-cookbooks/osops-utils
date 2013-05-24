e7b35d2a1a7f6ab3ea58bd93deac3c394160f131
Christopher H. Laco <claco@chrislaco.com>
2013-05-24 10:59:22 -0400

    Tailorize/Foodcritic metadata and fill in recipe/attributes
    Issue US336

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,37 @@
-maintainer       "Rackspace Hosting"
-maintainer_email "osops@lists.launchpad.net"
+name             "osops-utils"
+maintainer       "Rackspace US, Inc"
 license          "Apache 2.0"
-description      "Installs/Configures osops-utils"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+description      "Installs and configures osops-utils."
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.0.16"
 
-%w{ centos ubuntu }.each do |os|
+%w{ amazon centos debian fedora oracle redhat scientific ubuntu }.each do |os|
   supports os
 end
 
 %w{ apt sysctl yum }.each do |dep|
   depends dep
 end
+
+recipe "osops-utils::default",
+  "Installs rabbitmq related tcp settings in sysctl"
+
+recipe "osops-utils::packages",
+  "Installs various apt/yum repos"
+
+recipe "osops-utils::autoetchosts",
+  "Installs host file entries for all nodes in current chef environment"
+
+attribute "osops/apply_patches",
+  :description => "Enable/disable the application of patches",
+  :default => "false"
+
+attribute "osops/do_package_upgrades",
+  :description => "Enable/disable automatic package upgrades",
+  :default => "false"
+
+attribute "apt_repository/osops-packages",
+  :description => "Url of the osops packages repository"
+
+attribute "apt_repository/openstack",
+  :description => "Url of the openstack packages repository"
