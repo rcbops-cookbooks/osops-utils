28a554083d8292265510251acd4ba575a57ea5ed
cloudnull <kevin.carter@rackspace.com>
2013-10-03 09:55:27 -0500

    renamed client

--- recipes/python-quantumclient.rb
+++
@@ -1,6 +0,0 @@
-platform_options =  node['osops']['platform']
-
-package recipe_name do
-  action node["osops"]["do_package_upgrades"] == true ? :upgrade : :install
-  options platform_options["package_overrides"]
-end
