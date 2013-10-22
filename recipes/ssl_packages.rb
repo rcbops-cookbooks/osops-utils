f0fd74e921cb5418d627ebb0cd2d732fa3890d71
William Kelly <the.william.kelly@gmail.com>
2013-10-22 11:01:30 -0500

    s/package_overrides/package_options/g

--- recipes/ssl_packages.rb
+++ recipes/ssl_packages.rb
@@ -23,6 +23,6 @@ pkgs = platform_options["ssl_common_packages"]
 pkgs.each do |pkg|
   package pkg do
     action node["osops"]["do_package_upgrades"] == true ? :upgrade : :install
-    options platform_options["package_overrides"]
+    options platform_options["package_options"]
   end
 end
