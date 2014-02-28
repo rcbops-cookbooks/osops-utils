b82ff4588f3b3177bbff1934720257c72b005199
rackerjoe <breu@breu.org>
2014-02-28 09:54:08 -0600

    upgrade python-six on centos

--- recipes/python-novaclient.rb
+++ recipes/python-novaclient.rb
@@ -4,3 +4,12 @@ package recipe_name do
   action node["osops"]["do_package_upgrades"] == true ? :upgrade : :install
   options platform_options["package_options"]
 end
+
+case node["platform_family"]
+when "rhel"
+    # TODO(breu): remove this when CentOS packages aren't broken
+    # workaround for broken CentOS Packages.
+    package "python-six" do
+        action :upgrade
+    end
+end
